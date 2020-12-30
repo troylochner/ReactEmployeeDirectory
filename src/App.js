74925f6d0620733302d9e914a82e024e2d484518
troylochner <troy.lochner@gmail.com>
2020-12-30 14:02:58 -0500

    removed redundancy of the main component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from "react";
 //import React, {useState} from "react"; - Used in testing a new build
 import Wrapper from "./components/Wrapper";
 import Header from './components/Header';
-import Main from './components/Main';
 import Footer from './components/Footer';
+import EmployeeDirectory from './components/EmployeeDirectory'
+//import TestComp from './components/TestComp' // This is just a test component.
 // import Hello from "./sayHello"; just playing around and testing. 
-import TestComp from './components/TestComp' 
 
 //STEP BY STEP TESTS FOR MYSELF
 //THIS IS RENDERED ON LINE 7 of index.js
 //THIS THEN RENDERS ON OUR PUBLIC index.html ROOT DIV
-
 /*
 function App(){
  const [users, setUsers]= useState([
@@ -32,7 +31,7 @@ function App() {
       <div>
         <Wrapper>
         <Header /> 
-        <Main />  
+        <EmployeeDirectory />
         <Footer/>
         </Wrapper>
       </div>
